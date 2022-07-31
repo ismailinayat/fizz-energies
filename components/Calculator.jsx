58d0f4a7f4d35a20e71d264f8739493bb79f8e67
ismailinayat <ismailinayat@gmail.com>
2022-07-31 17:12:00 +0500

    improved custom calculator and implemented query for custom solution

--- components/Calculator.jsx
+++ components/Calculator.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 //import {FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
 
 const Calculator = () => {
@@ -10,42 +13,46 @@ const Calculator = () => {
   const [ac, setAc] = useState(0);
   const [washing, setWasshing] = useState(0);
   const [iron, setIron] = useState(0);
-  const [totalKV, setTotalKV] = useState(null);
+  const [totalWatts, setTotalWatts] = useState(null);
+  const [totalKv, setTotalKv] = useState(null);
+  const [popup, setPopup] = useState(false);
 
   const handleLightsChange = (e) => {
-    console.log(e.target.value);
+
     setBulbs(e.target.value);
   };
 
   const handleFanChange = (e) => {
-    console.log(e.target.value);
+
     setFans(e.target.value);
   };
 
   const handleTVChange = (e) => {
-    console.log(e.target.value);
+
     setTv(e.target.value);
   };
 
   const handleFridgeChange = (e) => {
-    console.log(e.target.value);
+
     setFridge(e.target.value);
   };
 
   const handleACChange = (e) => {
-    console.log(e.target.value);
+
     setAc(e.target.value);
   };
 
   const handleWashingChange = (e) => {
-    console.log(e.target.value);
+
     setWasshing(e.target.value);
   };
 
   const handleIronChange = (e) => {
-    console.log(e.target.value);
+
     setIron(e.target.value);
   };
+
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
@@ -57,19 +64,140 @@ const Calculator = () => {
     let totalWashingPower = parseInt(washing) * 500;
     let totalIronPower = parseInt(iron) * 1500;
 
-    setTotalKV(
-      totalBulbsPower +
-        totalFansPower +
-        totalTvPower +
-        totalFridgePower +
-        totalAcPower +
-        totalWashingPower +
-        totalIronPower
-    );
+    let totalWattsRequired = totalBulbsPower + totalFansPower + totalTvPower + totalFridgePower + totalAcPower + totalWashingPower + totalIronPower;
+
+    setTotalWatts(totalWattsRequired);
+
+    let kv = (totalWattsRequired + 1000) / 1000
+
+    console.log('kv', kv)
+
+
+    let decimal = kv.toString().split('.')[1];
+
+
+    
+    if (decimal) {
+      
+      decimal = decimal[0]
+    }
+    
+    console.log('decimal', decimal)
+
+    if (decimal >= 5) {
+
+      setTotalKv(Math.ceil(kv))
+    } else {
+      setTotalKv(Math.floor(kv))
+    }
+
+    //setTotalKv((totalWattsRequired + 1000) / 1000);
+
+    
   };
+
+  const closePopup = () => {
+
+    setPopup(false)
+
+  }
+
+  const openPopup = () => {
+
+    setPopup(true)
+  }
+
+  const sendQuery = (e) => {
+
+    e.preventDefault();
+    console.log('hello')
+    console.log(e.target)
+    e.target[3].innerText = 'Sending ...'
+    e.target[3].style.backgroundColor = 'grey'
+
+    emailjs.sendForm(
+        "service_qdm60xk",
+        "template_gz53em4",
+        
+        e.target,
+        "user_KGITFgaKwgk0uI96i7oZm"
+      )
+      .then(
+        (result) => {
+          e.target[3].innerText = 'Send Email';
+          e.target[3].style.backgroundColor = '#EA3A60'
+          toast.success("Email Sent!");
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+  }
 
   return (
     <div className="calculator__section">
+      <div className={ popup ? 'popup' : 'popup hidden'}>
+
+        <div className="popup__overlay" >
+          <div className="popup__content">
+
+
+            <div className="close" onClick={closePopup}>&times;</div>
+
+            <div className="popup__heading">
+              <h4>Please fill the form below and sends us the query. We will get back to you with our recommended solutions according to your requirements. </h4>
+            </div>
+            <div className="customer__query_details">
+              <div className="equipment"><h3>No of Bulbs: {bulbs}</h3></div>
+           
+              <div className="equipment"><h3>No of Fans: {fans}</h3></div>
+           
+              <div className="equipment"><h3>No of Televisions: {tv}</h3></div>
+          
+              <div className="equipment"><h3>No of Refregerators: {fridge}</h3></div>
+           
+              <div className="equipment"><h3>No of Air Conditioners: {ac}</h3></div>
+           
+              <div className="equipment"><h3>No of Washing Machines: {washing}</h3></div>
+           
+              <div className="equipment"><h3>No of Irons: {iron}</h3></div>
+            </div>
+          
+
+            <div className="form">
+              <form onSubmit={(e) => sendQuery(e)}>
+
+                <div className="details">
+                 
+                  <textarea id='message' name='Message' placeholder="Enter your message" required readOnly value={`Bulbs: ${bulbs}, Fans: ${fans}, Televisions: ${tv}, Refregerators: ${fridge}, Air Conditioners: ${ac}, Washing Machines: ${washing}, Irons: ${iron}}`} />
+                </div>
+
+                <div className="input">
+                  <label htmlFor="name">Please Enter Your Name:</label>
+                  <input  id='name' name='name' type="text" placeholder="Enter your Name" required />
+                </div>
+                <div className="input">
+                  <label htmlFor="email">Please Enter Your Mobile Number:</label>
+                  <input  id='number' name='Mobile' type="number" placeholder="Enter your mobile number" required />
+                </div>
+
+
+                  <button className="btn btn__green">Send Query</button>
+
+
+              </form>
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+
+      </div>
       <div className="calculator__container">
         <div className="calculator__section__heading">
           <h2>Want A More Customized Solution?</h2>
@@ -632,14 +760,19 @@ const Calculator = () => {
 
           <div className="totalPowerRequired">
             <h3>
-              {totalKV ? (
+              {totalWatts ? (
                 <p>
-                  You Require To Generate <span>{totalKV}</span> watts of
-                  energy. For this amount of energy you will need a 5KV unit.
+                  You Require To Generate <span>{totalWatts}</span> watts of
+                  energy. For this amount of energy we recommend that you will need a <span>{totalKv} Kv</span> unit.
                 </p>
               ) : null}
             </h3>
           </div>
+
+
+          {totalWatts ? (<div className="custom__query">
+            <button className="btn btn__green" onClick={openPopup}>Send Query For More Details</button>
+          </div>) : null }
         </div>
       </div>
     </div>
